0b7352aa34e268624aa08db5ca1f4210a9a2b17e
aiueo49 <ilikeski2007@yahoo.co.jp>
2024-04-23 21:12:35 +0900

    🚧 フォームまで

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,21 @@
+class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to root_path, notice: 'ユーザー登録に成功しました'
+    else
+      flash.now[:alert] = 'ユーザー登録に失敗しました'
+      render :new
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+  end
+end
