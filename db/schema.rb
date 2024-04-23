e5369ec629416cee2a06633593770246d114e369
aiueo49 <ilikeski2007@yahoo.co.jp>
2024-04-23 12:57:49 +0900

    ✨ 短歌を保管するためのポストテーブルを作成。

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_23_035223) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_23_035436) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_posts_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -23,4 +32,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_23_035223) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "posts", "users"
 end
