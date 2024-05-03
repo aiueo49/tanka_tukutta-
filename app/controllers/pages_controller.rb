b9ddf20081f020db11ea439ce594046918d8cc28
aiueo49 <ilikeski2007@yahoo.co.jp>
2024-05-04 01:00:49 +0900

    ✨ 利用規約ページの新規作成

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,4 @@
+class PagesController < ApplicationController
+  def terms
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -20,4 +20,5 @@ Rails.application.routes.draw do
     get 'current_user_page', on: :collection
   end
   resources :posts
+  get 'terms', to: 'pages#terms'
 end
