24c46f4e6227c6d2ff5e9fd147ce92a149ba41c5
aiueo49 <ilikeski2007@yahoo.co.jp>
2024-05-04 11:52:17 +0900

    🚧 OGPの設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,5 +8,33 @@ module ApplicationHelper
     else "bg-gray-500"
     end
   end
+
+  # OGPを設定する
+  def default_meta_tags
+    {
+      site: '短歌つくったー。',
+      title: '短歌生成サービス',
+      reverse: true,
+      charset: 'utf-8',
+      description: 'ユーザーが入力した文章をもとに短歌を生成するサービスです。',
+      keywords: '短歌, 生成, AI, テキスト, ユーザー, 入力, サービス',
+      canonical: request.original_url,
+      separator: '|',
+      og: {
+        site_name: '短歌つくったー。',
+        title: '短歌生成サービス',
+        description: 'ユーザーが入力した文章をもとに短歌を生成するサービスです。',
+        type: 'website',
+        url: request.original_url,
+        image: image_url('main_logo.png'),
+        locale: 'ja_JP',
+      },
+      twitter: {
+        card: 'summary_large_image',
+        site: '@',
+        image: image_url('main_logo.png')
+      }
+    }
+  end
 end
 
