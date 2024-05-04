b2567e9efd65a48aee9c429872eba9e59c0ab79b
aiueo49 <ilikeski2007@yahoo.co.jp>
2024-05-04 14:42:12 +0900

    🚧 独自ドメインを許可する設定を追記

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -3,6 +3,9 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Domain
+  config.hosts << 'www.tankatukutta.com'
+
   # Code is not reloaded between requests.
   config.enable_reloading = false
 
