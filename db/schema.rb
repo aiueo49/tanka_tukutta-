6ec5ba3f45495ef503cabb19f420e994aef7a0f6
aiueo49 <ilikeski2007@yahoo.co.jp>
2024-05-15 12:18:16 +0900

    🗃️ ポストテーブルに画像のURLを保存するためのカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_05_02_112953) do
+ActiveRecord::Schema[7.1].define(version: 2024_05_15_031643) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_05_02_112953) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.text "title"
+    t.text "image_url"
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
