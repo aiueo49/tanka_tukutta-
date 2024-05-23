2210769f791d3673eee77bef1c8bd918768ea274
aiueo49 <ilikeski2007@yahoo.co.jp>
2024-05-24 08:28:37 +0900

    ✅ ログインテスト改良

--- spec/requests/sessions_spec.rb
+++ spec/requests/sessions_spec.rb
@@ -8,7 +8,18 @@ RSpec.describe 'Sessions', type: :request do
   describe "POST /login" do
     it "ログインに成功すること" do
       post login_path, params: { email: @user.email, password: @user.password }
-      # ログインに成功した場合のみリダイレクトするので、リダイレクトされることを確認すればログインに成功したことがわかる
+      # 成功した場合のみリダイレクトするので、リダイレクトされることを確認すればログインに成功したことがわかる
+      expect(response).to have_http_status(:redirect)
+      follow_redirect!
+      expect(response.body).to include "ログインに成功しました"
+    end
+  end
+
+  describe "DELETE /logout" do
+    it "ログアウトに成功すること" do
+      post login_path, params: { email: @user.email, password: @user.password }
+      delete logout_path
+      # 成功した場合のみリダイレクトするので、リダイレクトされることを確認すればログアウトに成功したことがわかる
       expect(response).to have_http_status(:redirect)
     end
   end
