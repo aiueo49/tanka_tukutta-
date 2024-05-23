28310679b6ac48492a8c0ce45f68f6370db20384
aiueo49 <ilikeski2007@yahoo.co.jp>
2024-05-23 13:53:09 +0900

    🚸 初回アクセス時のみ、白背景にロゴが浮かぶように変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,18 @@
 class ApplicationController < ActionController::Base
+  helper_method :first_visit
+
   private
 
   def not_authenticated
     redirect_to login_path, alert: "恐れ入りますが、先にログインをお願いいたします。"
+  end
+
+  def first_visit
+    if session[:first_visit].nil?
+      session[:first_visit] = "false"
+      true
+    else
+      false
+    end
   end
 end
