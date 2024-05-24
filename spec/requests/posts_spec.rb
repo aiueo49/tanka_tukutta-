1c293afa9924b54243a9c1a047ed24a9c8bee738
aiueo49 <ilikeski2007@yahoo.co.jp>
2024-05-24 17:52:58 +0900

    ✅ 短歌作成が成功すること。（空白が完全一致の妨げになっていた）

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -20,7 +20,48 @@ RSpec.describe 'Post', type: :request do
 
   describe "POST /posts/select" do
     it "短歌が生成されること" do
-      # 外部APIの呼び出しをスタブ化
+      user_input = "ケーキが食べたいなあ"
+      predefind_response = "
+    # Prompt
+    背景情報：ユーザーが「#{user_input}」というをもとにして短歌を生成したいとリクエストしました。
+    短歌：「#{user_input}」という言葉をもとにして短歌を生成してください。
+    生成する短歌の内容：「#{user_input}」という言葉をもとにして短歌を生成してください。
+    生成する短歌の数：5パターン
+    生成する短歌の形式：5-7-5-7-7
+    生成する短歌の言葉の種類：「#{user_input}」という言葉をもとにして短歌を生成してください。
+    生成する短歌の言葉の難易度：中
+    生成する短歌の言葉の内容：「#{user_input}」という言葉をもとにして短歌を生成してください。
+    生成する短歌の言葉の感情：中立
+    生成する短歌の言葉のジャンル：一般
+    生成する短歌の言葉のトーン：中立
+    生成する短歌の言葉のスタイル：古語
+    生成する短歌の言葉のテーマ：一般
+    生成する短歌の言葉のトピック：一般
+    生成する短歌の言葉のキーワード：一般
+    生成する短歌の言葉のコンテキスト：一般
+    生成する短歌の言葉のコンセプト：一般
+    生成する短歌の言葉のアイデア：一般
+    生成する短歌の言葉の概念：一般
+    生成する短歌の言葉の構造：一般
+    生成する短歌の言葉の形：一般
+    生成する短歌の言葉のパターン：一般
+    生成する短歌の言葉のスキーム：一般
+    生成する短歌の言葉のフレーズ：日本の短歌に用いられるような、古風な言葉を使って短歌を生成してください。
+    生成する短歌の言葉のフレーム：句の音の数を以下のように指定します。生成した短歌をひらがなに書き換えた時の音の数が、5-7-5-7-7になるように短歌を生成してください。
+
+    # Example
+    ## input
+    user_input: ケーキが食べたいなあ
+    ## output
+    甘美なる 願いはケーキに 満たされぬ 舌を待ちわび 甘美の味を
+    求めしは 甘き果実の 心地に 空腹を癒し 舌鼓う時を
+    焼きたての 甘き蜜漬けの ケーキの 香りに誘われ 口舌乾かす
+    忘れがたき 甘きケーキの 味に酔い 舌鼓う日まで 待ちわびつつ
+    糖蜜に満つ 幸せの一刻 求めては 飢えたる魂が 舌を休めん
+    "
+
+      prompt = "#{user_input} + #{predefind_response}"
+
       stub_request(:post, "https://api.openai.com/v1/chat/completions")
         .with(
           body: {
@@ -28,8 +69,8 @@ RSpec.describe 'Post', type: :request do
             max_tokens: 512,
             temperature: 0.9,
             messages: [
-              { role: "system", content: "ケーキが食べたいなあ" },
-              { role: "user", content: "ケーキが食べたいなあ"}
+              { role: "system", content: prompt },
+              { role: "user", content: user_input }
             ]
           }.to_json,
           headers: {
@@ -39,13 +80,13 @@ RSpec.describe 'Post', type: :request do
         )
         .to_return(
           status: 200,
-          body: '{"choices":[{"text":"甘美なる 願いはケーキに 満たされぬ 舌を待ちわび 甘美の味を\n"}]}',
+          body: '{"choices":[{"message": {"content":"甘美なる 願いはケーキに 満たされぬ 舌を待ちわび 甘美の味を\n"}}]}',
           headers: { "Content-Type" => "application/json" }
         )
-      post select_posts_path, params: { title: "ケーキが食べたいなあ" }
+
+      post select_posts_path, params: { title: user_input }
       expect(response).to have_http_status(:success)
       expect(response.body).to include("甘美なる 願いはケーキに 満たされぬ 舌を待ちわび 甘美の味を")
     end
   end
 end
-
