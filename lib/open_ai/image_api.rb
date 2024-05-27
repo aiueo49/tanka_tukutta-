917a48626a1943d68a32ca16bf1dbf47a7387749
aiueo49 <ilikeski2007@yahoo.co.jp>
2024-05-27 21:08:28 +0900

    ✨ 画像を.pngから.webpで保存することに成功

--- lib/open_ai/image_api.rb
+++ lib/open_ai/image_api.rb
@@ -41,15 +41,31 @@ class OpenAi::ImageApi
   def upload_image_to_s3(image_url)
     s3 = Aws::S3::Resource.new
     bucket_name = ENV["S3_BUCKET_NAME"]
-    file_name = "generated_image_#{Time.now.to_i}.png"
+    # file名を.pngから.webpに変更
+    file_name = "generated_image_#{Time.now.to_i}.webp"
     obj = s3.bucket(bucket_name).object("uploads/#{file_name}")
 
     # Faradayを使用して画像をダウンロード
     response = Faraday.get(image_url)
     image_data = response.body
 
+    # 画像を一時ファイルに保存
+    temp_file = Tempfile.new(['temp_image', '.png'])
+    temp_file.binmode
+    temp_file.write(image_data)
+    temp_file.close
+
+    # 画像をwebp形式に変換
+    image = MiniMagick::Image.open(temp_file.path)
+    webp_image_path = temp_file.path.gsub('.png', '.webp')
+    image.format "webp"
+    image.write webp_image_path
+
     # S3へ画像をアップロード
-    obj.put(body: image_data)
+    obj.upload_file(webp_image_path)
+
+    # 一時ファイルを削除
+    temp_file.unlink
 
     # アップロードした画像のURLを返す
     obj.public_url
