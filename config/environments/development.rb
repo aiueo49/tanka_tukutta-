bd2077d3a62f9d1ae96e161bea68db251c73f63a
aiueo49 <ilikeski2007@yahoo.co.jp>
2024-07-07 21:14:25 +0900

    🚧 404ページの追加と開発環境でエラーページを見れるように設定（終わったら元に戻す）

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,8 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  # エラーハンドリングをローカル環境でテストしたいので追加、終わったらコメントアウトする
+  config.consider_all_requests_local = false
+  config.action_dispatch.show_exceptions = true
 end
